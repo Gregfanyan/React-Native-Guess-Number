5390bbf51f68561968a1ed90391ad8046ad5b94a
Gregfanyan <grigorfanyan@gmail.com>
2021-06-03 11:21:10 +0200

    feat: working with network image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -10,7 +10,15 @@ export default function GameOver(props) {
     <View style={styles.screen}>
       <TitleText>The Game is Over</TitleText>
       <View style={styles.imageContainer}>
-        <Image source={require("../assets/success.png")} style={styles.image} />
+        <Image
+          fadeDuration={200}
+          //source={require("../assets/success.png")}
+          source={{
+            uri: "https://image.shutterstock.com/image-photo/evening-view-ama-dablam-on-260nw-258841592.jpg",
+          }}
+          style={styles.image}
+          resizeMode="cover"
+        />
       </View>
       <BodyText>Number of Rounds: {props.roundsNumber} </BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
