ebd9f021509de4e39ba4b8674912016f93e23f90
Gregfanyan <grigorfanyan@gmail.com>
2021-06-03 11:08:13 +0200

    feat: adding local image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -9,9 +9,11 @@ export default function GameOver(props) {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over</TitleText>
+      <View style={styles.imageContainer}>
+        <Image source={require("../assets/success.png")} style={styles.image} />
+      </View>
       <BodyText>Number of Rounds: {props.roundsNumber} </BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
-      <Image source={require('../assets/favicon.png')} />
       <Button title="New Game" onPress={props.onRestart} />
     </View>
   );
@@ -21,5 +23,18 @@ const styles = StyleSheet.create({
   screen: {
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
